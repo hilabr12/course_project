f64eb24becdf351b99f13b02727b059c87e706ef
hila <hilabr8@gmail.com>
2024-05-11 12:41:12 +0300

    pythagorean_triplet_by_sum solved

--- mamas_course_project_python/pythagorean_triplet_by_sum.py
+++ mamas_course_project_python/pythagorean_triplet_by_sum.py
@@ -1,5 +1,20 @@
-import math
+from math import pow
 
 
-def pythagorean_triplet_by_sum(sum: int):
-    print("")
+def pythagorean_triplet_by_sum(sum_triplet: int) -> None:
+    a = 1
+    while a < sum_triplet:
+        b = a + 1
+        while b < sum_triplet:
+            c = sum_triplet - a - b
+            if pow(a, 2) + pow(b, 2) == pow(c, 2):
+                print(f"{a} < {b} < {c}")
+            b += 1
+        a += 1
+
+
+pythagorean_triplet_by_sum(12)
+pythagorean_triplet_by_sum(30)
+pythagorean_triplet_by_sum(40)
+pythagorean_triplet_by_sum(56)
+pythagorean_triplet_by_sum(24)

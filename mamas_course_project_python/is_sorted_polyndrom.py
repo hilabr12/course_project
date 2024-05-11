2f0bf35005a8a4f5b286abe35cc8f4c5119ed67a
hila <hilabr8@gmail.com>
2024-05-11 13:45:32 +0300

    is_sorted_palindrom function is working, need to make it shorter

---
+++ mamas_course_project_python/is_sorted_polyndrom.py
@@ -0,0 +1,31 @@
+ODD = 1
+
+
+def is_odd_number(word: str) -> bool:
+    return word % 2 == 1
+
+
+def is_word_sorted_in_alphabetical_order(word: str) -> bool:
+    is_sorted = all([word[i] <= word[i+1] for i in range(len(word) - 1)])
+    return is_sorted
+
+
+def is_sorted_polyndrom(word: str) -> bool:
+    half_length_of_word = len(word) // 2
+    first_sliced_word: str = word[:half_length_of_word]
+    second_sliced_word: str = word[half_length_of_word + 1:][::-1]
+
+    if is_odd_number:
+        remaining_char = word[half_length_of_word]
+        first_sliced_word += remaining_char
+        second_sliced_word += remaining_char
+
+    if is_word_sorted_in_alphabetical_order(first_sliced_word):
+        if is_word_sorted_in_alphabetical_order(second_sliced_word):
+            if first_sliced_word == second_sliced_word:
+                return True
+    return False
+
+
+print(is_sorted_polyndrom("abcdcba"))
+print(is_sorted_polyndrom("AbcdCbA"))

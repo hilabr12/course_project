8781bb95370fa0d5fa61bcf63781afe0cf044e35
hila <hilabr8@gmail.com>
2024-05-12 19:34:38 +0300

    created class node

---
+++ excersices_c_sharp/excersices_c_sharp/Node.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace excersices_c_sharp
+{
+    public class Node
+    {
+        public int Value {  get; set; }
+        public Node Next { get; set; }
+        public Node(int value, Node next = null) 
+        { 
+            Value = value;
+            Next = next;
+        }
+
+
+    }
+}

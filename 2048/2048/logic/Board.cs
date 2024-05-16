b0293bd75b4a7636ebf254266d1738326aac3e01
hila <hilabr8@gmail.com>
2024-05-16 21:24:45 +0300

    added enum direction

--- 2048/2048/logic/Board.cs
+++ 2048/2048/logic/Board.cs
@@ -1,12 +1,12 @@
 ﻿using System;
-
+using System.Linq;
 namespace _2048.logic
 {
     public class Board
     {
         internal const int BOARD_SIZE_ROW = 4;
         internal const int BOARD_SIZE_COLUMN = 4;
-        internal int[,] BoardPoints;
+        internal Cell[,] BoardPoints;
         public static readonly int[] RANDOM_VALUES_OPTIONS = { 2, 4 };
         Random rnd = new Random();
 
@@ -16,10 +16,14 @@ namespace _2048.logic
             BoardPoints = InitializeBoard();
         }
 
-
+         
         public int[,] InitializeBoard()
         {
-
+            Random rnd = new Random();
+            int firstRandomNewCellValue = RANDOM_VALUES_OPTIONS.ElementAt(rnd.Next(RANDOM_VALUES_OPTIONS.Length));
+            int secondRandomNewCellValue = RANDOM_VALUES_OPTIONS.ElementAt(rnd.Next(RANDOM_VALUES_OPTIONS.Length));
+            Cell[,] board = new Cell[BOARD_SIZE_ROW, BOARD_SIZE_COLUMN];
+            
 
         }
 
